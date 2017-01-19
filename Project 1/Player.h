217b408f09c35cbf7723a3d3ae92d0873be57309
laofapotato <zhong74.langhao@hotmail.com>
2017-01-19 14:38:05 -0800

    Create Player.h

---
+++ Project 1/Player.h
@@ -0,0 +1,34 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+using namespace std;
+
+
+class Arena;
+
+class Player
+{
+public:
+	// Constructor
+	Player(Arena* ap, int r, int c);
+
+	// Accessors
+	int  row() const;
+	int  col() const;
+	bool isDead() const;
+
+	// Mutators
+	string dropPoisonPellet();
+	string move(int dir);
+	void   setDead();
+
+private:
+	Arena* m_arena;
+	int    m_row;
+	int    m_col;
+	bool   m_dead;
+};
+
+#endif  //PLAYER_H
